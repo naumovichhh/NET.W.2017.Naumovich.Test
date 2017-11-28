41538fc617d438daec0dee3110cce068431bed48
naumovichhh <32872842+naumovichhh@users.noreply.github.com>
2017-11-28 12:24:44 +0300

    Add files via upload

---
+++ Task4.Solution/AveragingMethod.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace Task4.Solution
+{
+    public static class AveragingMethods
+    {
+        public static double Mean(List<double> values)
+        {
+            return values.Sum() / values.Count;
+        }
+
+        public static double Median(List<double> values)
+        {
+            var sortedValues = values.OrderBy(x => x).ToList();
+
+            int n = sortedValues.Count;
+
+            if (n % 2 == 1)
+            {
+                return sortedValues[(n - 1) / 2];
+            }
+
+            return (sortedValues[sortedValues.Count / 2 - 1] + sortedValues[n / 2]) / 2;
+        }
+    }
+}
---
+++ Task4.Solution/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task4.Solution
+{
+    public delegate double AveragingMethod(List<double> list);
+
+    public class Calculator
+    {
+        public double CalculateAverage(List<double> values, AveragingMethod averagingMethod)
+        {
+            if (values == null)
+            {
+                throw new ArgumentNullException(nameof(values));
+            }
+
+            if (averagingMethod == null)
+            {
+                throw new ArgumentNullException(nameof(averagingMethod));
+            }
+
+            return averagingMethod(values);
+        }
+    }
+}
